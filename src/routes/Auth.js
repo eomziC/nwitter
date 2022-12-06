0cc68fff3af93d01895801d327bc615744eb711d
Troy <yoon6@gmail.com>
2022-12-06 09:40:25 +0900

    upload files

---
+++ src/routes/Auth.js
@@ -0,0 +1,83 @@
+import React, {useState} from "react";
+import {authService, firebaseInstance} from "../fbase";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    GithubAuthProvider,
+    GoogleAuthProvider,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+} from "firebase/auth";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
+    const onChange = (e) => {
+        const {name, value} = e.target;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            let data
+            const auth = getAuth()
+            if (newAccount) {
+                data = await createUserWithEmailAndPassword(auth, email, password);
+            } else {
+                data = await signInWithEmailAndPassword(auth, email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+    const onSocialClick = async (event) => {
+        const {
+            target: { name },
+        } = event;
+        let provider;
+        try {
+            if (name === "google") {
+                provider = new GoogleAuthProvider();
+                const result = await signInWithPopup(authService, provider);
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+// const token = credential.accessToken;
+            } else if (name === "github") {
+                provider = new GithubAuthProvider();
+                const result = await signInWithPopup(authService, provider);
+                const credential = GithubAuthProvider.credentialFromResult(result);
+// const token = credential.accessToken;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
+                <input name="password" type="password" placeholder="Password" required value={password}
+                       onChange={onChange}/>
+                <input type="submit" value={newAccount ? "Create Account" : "Sign In"}/>
+                {error}
+            </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+            <div>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with Github</button>
+            </div>
+        </div>
+    );
+
+};
+export default Auth;
---
+++ src/routes/EditProfile.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Auth from "./Auth";
+
+const EditProfile= () => <span>Edit Profile</span>
+export default EditProfile;
